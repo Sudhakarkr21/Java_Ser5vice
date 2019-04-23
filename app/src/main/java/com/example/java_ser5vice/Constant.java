de074a8be84fbac1cafc38a6c35e79b1d343432c
Sudhakarkr21 <sudhakarkr21@gmail.com>
2019-04-23 13:17:32 +0530

    Second code

--- app/src/main/java/com/example/java_ser5vice/Constant.java
+++ app/src/main/java/com/example/java_ser5vice/Constant.java
@@ -3,6 +3,6 @@ package com.example.java_ser5vice;
 public class Constant {
     public static final int LOGIN = 1;
     public static final int LOGIN_FAILURE = 2;
-    public static final int DELETE = 3;
+    public static final int DELETE = 55;
     public static final int NOT_DELETED = 4;
 }
